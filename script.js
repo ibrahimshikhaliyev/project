a315360383599330fed05f2c7568bbd4ed7949e8
ibra <shikhaliyevibrahim1@gmail.com>
2022-02-02 23:45:14 +0400

    personal movie db kak sozdat

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 "use strict";
 //~~~!!!@@@###$$^%%%^^&&&****(((((((()))))))___+++}}{{{|||}}}
+const numberOfFilms=+prompt("how much films did u wantched?","");
 
-let item1=true
-   ,item2=false;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        
+    },
+    actors: {},
+    genres: [],
+    privat:false
 
-console.log(item1||item2);
-
-
+};
+const lastFilm1 = prompt("what is the last film that u watched?" ,""),
+      rate1=+prompt("how would u rate it?" ,""),
+      lastFilm2= prompt("what is the last film that u watched?" ,""),
+      rate2=+prompt("how would u rate it?" ,"")
+;
+personalMovieDB.movies[lastFilm1]=rate1;
+personalMovieDB.movies[lastFilm2]=rate2;
+console.log(personalMovieDB);
